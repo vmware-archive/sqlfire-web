cbfc9e7a55ac45f13a2a3b715a8830c577df817a
Pas Apicella <papicella@vmware.com>
2013-04-23 20:33:56 +1000

    Fix index issues wit SQLFire 1.1

--- vmware-au-se-sqlfireweb/src/main/java/vmware/au/se/sqlfireweb/dao/indexes/Constants.java
+++ vmware-au-se-sqlfireweb/src/main/java/vmware/au/se/sqlfireweb/dao/indexes/Constants.java
@@ -3,17 +3,17 @@ package vmware.au.se.sqlfireweb.dao.indexes;
 public interface Constants 
 {
 
-	public static final String USER_INDEXES = 
-			"select distinct index, \"UNIQUE\", \"TABLE\", \"INDEX_TYPE\", \"COLUMNS_AND_ORDER\", \"SCHEMA\" " +
-			"from   sys.indexes " +
-			"where  \"SCHEMA\" = ? " +
-			"and index like ?";
-
 //	public static final String USER_INDEXES = 
-//			"select distinct indexname, \"UNIQUE\", tablename, indextype, COLUMNS_AND_ORDER, schemaname " +
+//			"select distinct index, \"UNIQUE\", \"TABLE\", \"INDEX_TYPE\", \"COLUMNS_AND_ORDER\", \"SCHEMA\" " +
 //			"from   sys.indexes " +
-//			"where  schemaname = ? " +
-//			"and indexname like ?";
+//			"where  \"SCHEMA\" = ? " +
+//			"and index like ?";
+
+	public static final String USER_INDEXES = 
+			"select distinct indexname, \"UNIQUE\", tablename, indextype, COLUMNS_AND_ORDER, schemaname " +
+			"from   sys.indexes " +
+			"where  schemaname = ? " +
+			"and indexname like ?";
 
 	
 	public static String DROP_INDEX = "drop index %s.\"%s\"";
